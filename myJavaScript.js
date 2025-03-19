0252d1467eaa7c52988eaf82d0dd3caf1acd4a15
eliasehret <elias.ehret@gmail.com>
2025-03-19 23:12:43 +0100

    Update myJavaScript.js

--- myJavaScript.js
+++ myJavaScript.js
@@ -28,3 +28,52 @@ function openProbetrainingsAnmeldung(){
         sideBarOpen = true;
     }
 }
+
+var g = document.getElementById("Kontaktformular");
+
+
+document.getElementById("Kontaktformular").addEventListener("submit", async function(event){
+    event.preventDefault();
+
+    console.log("log");
+    // Daten für den JSON-Body
+    const data = {
+        elternName: event.target.ElternName.value,
+        kindName: event.target.KindName.value,
+
+        email: event.target.email.value,
+        telefonnummer: event.target.Telefonnummer.value,
+
+        kurs: event.target.kurs.value
+    };
+
+    var url = "";
+
+    alert("fetch");
+    return;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(response.ok) {
+            alert("Formular erfolgreich übermittelt!");
+
+            // Clear the form here
+            document.getElementById("Kontaktformular").reset();
+
+            location.reload();
+        } else {
+            const errorData = await response.json();
+            alert("Fehler bei der Übermittlung: " + JSON.stringify(errorData));
+        }
+    } catch (error) {
+        alert("Fehler bei der Übermittlung: " + error.message);
+    }
+});
+
